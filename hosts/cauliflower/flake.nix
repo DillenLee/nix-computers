18e4a14de4d005d899eda14af6680a7b206a6866
Dillen Lee <dillenmylee@gmail.com>
2025-11-02 15:14:05 +0000

    added flak

---
+++ hosts/cauliflower/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+	nixosConfigurations.Broccoli= lib.nixosSystem {
+	  inherit system;
+	  modules = [ ./configuration.nix ];
+	};
+
+      homeConfigurations = {
+	  dillen = home-manager.lib.homeManagerConfiguration {
+	    inherit pkgs;
+	    modules = [
+	    ./home.nix
+	    nixvim.homeModules.nixvim
+	    ];
+	  };
+	};
+      };
+}
+
