ddaf6141edd8d7db4548d9114401fedf06bd2ac0
Dillen Lee <dillenmylee@gmail.com>
2025-12-22 18:19:23 +0100

    updated flake

--- hosts/edamame/flake.nix
+++ hosts/edamame/flake.nix
@@ -1,21 +1,41 @@
 {
-  description = "A very basic flake";
+	description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager/master";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		nixvim = {
+			url = "github:nix-community/nixvim";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs = { self, nixpkgs }: 
-  let 
-    system = "x86_64-linux";
-    lib = nixpkgs.lib;
-  in
-  {
-    nixosConfigurations = {
-       nixos = lib.nixosSystem {  
-         inherit system;
-         modules = [ ./configuration.nix ];
-       };
-    };
-  };
+	outputs = { self, nixpkgs, home-manager, nixvim, ... }:
+		let
+		lib = nixpkgs.lib;
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+		nixosConfigurations.cauliflower= lib.nixosSystem {
+			inherit system;
+			modules = [ 
+				./configuration.nix 
+			];
+		};
+
+		homeConfigurations = {
+			dillen = home-manager.lib.homeManagerConfiguration {
+				inherit pkgs;
+				modules = [
+					./home.nix
+						nixvim.homeModules.nixvim
+				];
+			};
+		};
+	};
 }
+
