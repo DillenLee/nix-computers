94e86ed66d582c4e0d14723accf6c85a586fcbe9
Dillen <dillenmylee@gmail.com>
2026-01-05 13:58:25 +0100

    plugins.lsp -> lsp

--- modules/nixvim/lsp.nix
+++ modules/nixvim/lsp.nix
@@ -1,15 +1,40 @@
 {...}:
 {
-	programs.nixvim.plugins.lsp = {
-			enable = true;
+	programs.nixvim = {
+		lsp = {
+				enable = true;
 
-			servers = {
-				ruff.enable = true; # Python linter
-				jedi_language_server.enable = true; # Python goto
-				# julials.enable = true; # Julia
-				clangd.enable = true; # C/C++
-				# arduino_language_server.enable = true;
-				nixd.enable = true;
-			};
+				keymaps = [
+					{
+						key = "gd";
+						lspBufAction = "definition";
+					}
+					{
+						key = "gD";
+						lspBufAction = "references";
+					}
+					{
+						action = {
+							__raw =
+							"require('telescope.builtin').lsp_definitions";
+							};
+						key = "gd";
+					}
+				];
+				
+				servers = {
+					ruff.enable = true; # Python linter
+					jedi_language_server.enable = true; # Python goto
+					# julials.enable = true; # Julia
+					clangd.enable = true; # C/C++
+					# arduino_language_server.enable = true;
+					nixd.enable = true;
+				};
+		};
+
+		# Required for now to ensure that the servers have default values.
+		# Probably expect this to be removed soon.
+		plugins.lspconfig.enable = true;
+
 	};
 }
